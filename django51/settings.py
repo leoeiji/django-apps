bb8b929c562d5b2e2c4ce5efe5175961b3f98e55
Leo Eiji <leoeiji1234@gmail.com>
2022-06-24 17:09:51 -0300

    Big Bang!

--- django51/settings.py
+++ django51/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = "django51.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "databases" / "db.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
