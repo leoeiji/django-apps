a54e98c6cfebafd30b9327cb85d5ba099ef896dd
Leo Eiji <leoeiji1234@gmail.com>
2022-06-26 13:41:28 -0300

    Infrastructure began

--- backend/django51/settings.py
+++ backend/django51/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -82,11 +83,15 @@ WSGI_APPLICATION = "django51.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
+print("aaaaaaaaaaaaaaaaaaaaa", os.environ.get("MYSQL_USER"))
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get("MYSQL_DB"),
+        'USER': os.environ.get("MYSQL_USER"),
+        'PASSWORD': os.environ.get("MYSQL_ROOT_PASSWORD"),
+        'HOST': '127.0.0.1',
+        'PORT': '3307',
     }
 }
 
